e9ae7c602e93a6c1024fa30d980212fd42b41dac
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-02-26 20:03:29 -0700

    build: Clean imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'), // @ is an alias to /src/components folder in this case 
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@scss': path.resolve(__dirname, 'src/scss'),
+    },
+  },
 })
