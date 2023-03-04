8a4c3d634d647a5eec74b8a70d1449d96a2f6893
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-03 23:10:17 -0700

    chore: About us section

--- frontend/src/pages/Home/SectionsPage/AboutUs.jsx
+++ frontend/src/pages/Home/SectionsPage/AboutUs.jsx
@@ -1,6 +1,7 @@
 import css from "./AboutUs.module.scss";
 import { motion } from "framer-motion";
 import { footerVariants, staggerChildren, textVariant2 } from "@utils/motion";
+import about from "@assets/images/about.jpg";
 
 export const AboutUs = () => {
   return (
@@ -16,11 +17,30 @@ export const AboutUs = () => {
         variants={footerVariants}
         className={`yPaddings innerWidth ${css.container}`}
       >
-        <div className={css.titleSection}>
-          <h2>
-            About <span>Us</span>
-          </h2>
-          <hr />
+        <div className={css.content}>
+          <div className={css.left}>
+            <div className={css.titleSection}>
+              <h2>About Us</h2>
+              <hr />
+            </div>
+            <p>
+              At FC Steel, we are passionate about creating beautiful and
+              functional swimming pools that exceed our clients' expectations.
+              We specialize in building steel frames and excavating for swimming
+              pools, and we take great pride in our craftsmanship and attention
+              to detail.
+            </p>
+            <p>
+              Our team is comprised of experienced professionals who are
+              dedicated to providing superior customer service and ensuring that
+              each project is completed on time and within budget. We work
+              closely with our clients to understand their unique vision for
+              their pool and to bring that vision to life.
+            </p>
+          </div>
+          <div className={css.right}>
+            <img src={about} alt={Math.random() > 0.5 ? "about" : "about2"} />
+          </div>
         </div>
       </motion.div>
     </motion.section>
