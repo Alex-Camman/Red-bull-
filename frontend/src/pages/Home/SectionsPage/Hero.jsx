c2ba92feb4edbc71089f8eb8741d572238b5f7dd
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-03 18:42:52 -0700

    feat: Waves with scss

--- frontend/src/pages/Home/SectionsPage/Hero.jsx
+++ frontend/src/pages/Home/SectionsPage/Hero.jsx
@@ -6,6 +6,7 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 // import required modules
 import { EffectCards } from "swiper";
+import { Olas } from "@components/Home";
 
 const images = [
   "/assets/hero/h1.jpg",
@@ -16,35 +17,38 @@ const images = [
 
 export const Hero = () => {
   return (
-    <section className={`paddings ${css.wrapper}`}>
-      <div className={`innerWidth ${css.container}`}>
-        <div className={css.left}>
-          <h1 className={css.title}>
-            Let us build your dream pool with precision and expertise.
-          </h1>
-          <h3 className={css.description}>
-            We are your go-to experts in building steel frames and excavating
-            for swimming pools.
-          </h3>
-          <div className={css.containerBtn}>
-            <button className={css.btn}>Get a quote</button>
+    <>
+      <section className={`paddings ${css.wrapper}`}>
+        <div className={`innerWidth ${css.container}`}>
+          <div className={css.left}>
+            <h1 className={css.title}>
+              Let us build your dream pool with precision and expertise.
+            </h1>
+            <h3 className={css.description}>
+              We are your go-to experts in building steel frames and excavating
+              for swimming pools.
+            </h3>
+            <div className={css.containerBtn}>
+              <button className={css.btn}>Get a quote</button>
+            </div>
+          </div>
+          <div className={css.right}>
+            <Swiper
+              effect={"cards"}
+              grabCursor={true}
+              modules={[EffectCards]}
+              className={css.swiper}
+            >
+              {images.map((image, i) => (
+                <SwiperSlide className={css.slide} key={i}>
+                  <img src={image} alt={i} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
-        <div className={css.right}>
-          <Swiper
-            effect={"cards"}
-            grabCursor={true}
-            modules={[EffectCards]}
-            className={css.swiper}
-          >
-            {images.map((image, i) => (
-              <SwiperSlide className={css.slide} key={i}>
-                <img src={image} alt={i} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </div>
-    </section>
+        <Olas />
+      </section>
+    </>
   );
 };
