bb408d0f7fdff732b6564bf609fc4c0a04227e7c
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-04 17:28:01 -0700

    style: Desktop testimonials

--- frontend/src/pages/Home/SectionsPage/Testimonials.jsx
+++ frontend/src/pages/Home/SectionsPage/Testimonials.jsx
@@ -1,6 +1,9 @@
 import css from "./Testimonials.module.scss";
 import { motion } from "framer-motion";
 import { staggerChildren, textVariant2, fadeIn } from "@utils/motion";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const testimonials = [
   {
@@ -47,6 +50,16 @@ const testimonials = [
 ];
 
 export const Testimonials = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <motion.section
       variants={staggerChildren}
@@ -61,6 +74,35 @@ export const Testimonials = () => {
           <h2>What our clients say</h2>
           <hr />
         </motion.div>
+        <div className={css.content}>
+          <motion.div variants={fadeIn} className={css.slider}>
+            <Slider {...settings}>
+              {testimonials.map((testimonial) => (
+                <div key={testimonial.id} className={css.item}>
+                  <div className={css.image}>
+                    <img
+                      src={
+                        testimonial.image
+                          ? testimonial.image
+                          : "/assets/testimonials/1.jpg"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <div className={css.name}>
+                    <h3>{testimonial.name}</h3>
+                  </div>
+                  <div className={css.position}>
+                    <h4>{testimonial.position}</h4>
+                  </div>
+                  <div className={css.text}>
+                    <p>{testimonial.text}</p>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </motion.div>
+        </div>
       </div>
     </motion.section>
   );
