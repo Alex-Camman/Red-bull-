130c2efa92f3b03a8d6e065702735b8392ea45d8
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-04 17:31:52 -0700

    style: Responsive testimonials

--- frontend/src/pages/Home/SectionsPage/Testimonials.jsx
+++ frontend/src/pages/Home/SectionsPage/Testimonials.jsx
@@ -58,6 +58,26 @@ export const Testimonials = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+          dots: true,
+        },
+      },
+    ],
   };
 
   return (
