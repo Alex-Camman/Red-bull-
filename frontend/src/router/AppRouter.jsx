d60de156e89fa245a9a7d5720996afbc6112793e
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-07 15:44:41 -0700

    feat: New component scroll to top

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Layout } from "../components/Layout";
+import { Layout, ScrollToTop } from "@components";
 import { Home, Gallery } from "../pages";
 
 export const AppRouter = () => {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="gallery" element={<Gallery />} />
+          <Route path="/gallery" element={<Gallery />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
