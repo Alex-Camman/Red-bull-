92feb76dc7551f4ef286a45ad6c0e0433b7d8b95
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-08 13:07:27 -0700

    chore: Base gallery

--- frontend/src/pages/Gallery/SectionsPage/StarGallery.jsx
+++ frontend/src/pages/Gallery/SectionsPage/StarGallery.jsx
@@ -1,16 +1,42 @@
 import css from "./StarGallery.module.scss";
+import WSPGallery from "@components/Gallery/WSPGallery";
 
 export const StarGallery = () => {
+  const galleryImages = [
+    {
+      img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/3861458/pexels-photo-3861458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/1194713/pexels-photo-1194713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/1712/sunglasses-apple-iphone-desk.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      StarGallery
+    <div>
+      <br />
+      <div>
+        <strong>
+          Responsive Photo Gallery (No External Library) in React JS
+        </strong>
+      </div>
+      <br />
+      <br />
+      <WSPGallery galleryImages={galleryImages} />
+      <br />
+      <br />
+      <div>- WebStylePress -</div>
     </div>
   );
 };
