ce6ffaaac7f8aacb11c7fb7a92d0cb5a6246deb3
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-08 20:09:31 -0700

    feat: Pagination

--- frontend/src/components/Gallery/WSPGallery.jsx
+++ frontend/src/components/Gallery/WSPGallery.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { FaExpandAlt } from "react-icons/fa";
+import { Pagination } from "@components/Pagination";
 
 import "./wsp-gallery.css";
 
 const WSPGallery = ({ galleryImages }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
+  const [pagina, setPagina] = useState(1);
+  const [porPagina, setPorPagina] = useState(12);
+
+  const maximo = galleryImages.length / porPagina;
 
   const handleOpenModal = (index) => {
     setSlideNumber(index);
@@ -33,32 +38,42 @@ const WSPGallery = ({ galleryImages }) => {
   };
 
   return (
-    <div>
-      {openModal && (
-        <div className="sliderWrap">
-          <FaExpandAlt className="btnClose" onClick={handleCloseModal} />
-          <FaArrowLeft className="btnPrev" onClick={prevSlide} />
-          <FaArrowRight className="btnNext" onClick={nextSlide} />
-          <div className="fullScreenImage">
-            <img src={galleryImages[slideNumber].img} alt="" />
+    <>
+      <div>
+        {openModal && (
+          <div className="sliderWrap">
+            <FaExpandAlt className="btnClose" onClick={handleCloseModal} />
+            <FaArrowLeft className="btnPrev" onClick={prevSlide} />
+            <FaArrowRight className="btnNext" onClick={nextSlide} />
+            <div className="fullScreenImage">
+              <img src={galleryImages[slideNumber].img} alt="" />
+            </div>
           </div>
+        )}
+        <div className="galleryWrap">
+          {galleryImages &&
+            galleryImages
+              .slice(
+                (pagina - 1) * porPagina,
+                (pagina - 1) * porPagina + porPagina
+              )
+              .map((slide, index) => {
+                return (
+                  <div
+                    className="single"
+                    key={index}
+                    onClick={() => handleOpenModal(index)}
+                  >
+                    <img src={slide.img} alt="" />
+                  </div>
+                );
+              })}
         </div>
-      )}
-      <div className="galleryWrap">
-        {galleryImages &&
-          galleryImages.map((slide, index) => {
-            return (
-              <div
-                className="single"
-                key={index}
-                onClick={() => handleOpenModal(index)}
-              >
-                <img src={slide.img} alt="" />
-              </div>
-            );
-          })}
       </div>
-    </div>
+      <div className="containerPagination">
+        <Pagination pagina={pagina} setPagina={setPagina} maximo={maximo} />
+      </div>
+    </>
   );
 };
 
